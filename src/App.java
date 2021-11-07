b349103d1be9bd265900bc4f984a74543cfd5463
Nikitaeloxin <nikitaeloxin@yandex.ru>
2021-11-07 10:50:59 +0200

    commit2

--- src/App.java
+++ src/App.java
@@ -11,20 +11,22 @@ public class App {
         words.add("Tractor");
 
         int indexBuf = (int)(Math.random()*(words.size()-1));
-        String guesLetter;
-        Scanner in = new Scanner(System.in);
-        char [] buffer = words.get(indexBuf).toCharArray();
+        String guesLetter;   // input char that gues letter
+        
+        char [] buffer = words.get(indexBuf).toCharArray(); // word with whose you work
         int count = 0;
 
         for(int i = 0; i <words.get(indexBuf).length();i++ ){
+            //define buffer word like "_______"
             buffer[i] = '_';
         }
         
-        char [] wordBuf = words.get(indexBuf).toCharArray();
-        
+        char [] wordBuf = words.get(indexBuf).toCharArray(); // the word to be compared with
+        Scanner in = new Scanner(System.in);
+
         while (buffer != words.get(indexBuf).toCharArray() || count != 5){
             guesLetter = in.next();
-            char[] chGuesLetter = guesLetter.toCharArray();
+            char[] chGuesLetter = guesLetter.toCharArray(); //convert letter from string to char
 
             for(int i = 0; i <words.get(indexBuf).length();i++ ){
                 if (chGuesLetter[0] == words.get(indexBuf).charAt(i)){
